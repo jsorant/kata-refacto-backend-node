63322998c2edb025e77edf7108c2f315d0e5f1ac
Jérémy Sorant <jsorant@gmail.com>
2025-10-10 23:49:23 +0200

    chore: Update deps (node 24 & vitest)

--- typescript/vitest.config.ts
+++ typescript/vitest.config.ts
@@ -7,5 +7,36 @@ export default defineConfig({
             enabled: true,
             reporter: ['text', 'html'],
         },
+        projects: [
+            'packages/*',
+            {
+                extends: true,
+                test: {
+                    include: ['tests-e2e/**/*.spec.ts'],
+                    name: 'e2e',
+                }
+            },
+            {
+                extends: true,
+                test: {
+                    include: ['tests-integration/**/*.spec.ts'],
+                    name: 'integration',
+                }
+            },
+            {
+                extends: true,
+                test: {
+                    include: ['tests-component/**/*.spec.ts'],
+                    name: 'component',
+                }
+            },
+            {
+                extends: true,
+                test: {
+                    include: ['tests-unit/**/*.spec.ts'],
+                    name: 'unit',
+                }
+            }
+        ]
     },
 });
